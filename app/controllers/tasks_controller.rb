1b24a2aeb48e54eb514136bfdad440af3a2a10d0
Pierre Axel Izerable <pierreaxel.iz@gmail.com>
2024-07-02 22:47:21 +0200

    maj code

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,6 +20,25 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task), notice: 'Task was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, notice: 'Task was successfully deleted.'
+  end
+
   private
 
   def task_params
